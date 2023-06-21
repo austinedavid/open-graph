013f6f07779cfdb5c5d6630c65025cf35bffff1f
david <austinedavid96@gmail.com>
2023-06-21 23:08:58 +0100

    running the ogs now

--- src/app/about/[slug]/page.tsx
+++ src/app/about/[slug]/page.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import {Metadata} from "next"
 
+export const metadata:Metadata = {
+  title: "GmodeTech about",
+  description: "Gmodetech about description here",
+  openGraph:{
+    title: "Gmode Opengraph about",
+    description: "our about description for the main one here"
+  }
+}
 const page = ({params:{slug}}:{params:{slug:string}}) => {
 
   return (
--- src/app/service/[slug]/page.tsx
+++ src/app/service/[slug]/page.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import {Metadata} from "next"
 
+export const metadata:Metadata = {
+  title: "GmodeTech service",
+  description: "Gmodetech is one of the best when it comes to service running in Nigeria and oversea",
+  openGraph:{
+    title: "Gmode Opengraph service",
+    description: "our service description for the main one here"
+  }
+}
 const page = () => {
   return (
     <div>this is the service page here</div>

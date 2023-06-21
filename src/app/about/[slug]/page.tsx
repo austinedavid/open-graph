f98eb5d2aef0b3abebfb80ca6ddc9675166be1b6
david <austinedavid96@gmail.com>
2023-06-21 05:36:08 +0100

    added the git

--- src/app/about/[slug]/page.tsx
+++ src/app/about/[slug]/page.tsx
@@ -5,6 +5,7 @@ const page = ({params:{slug}}:{params:{slug:string}}) => {
   return (
     <div>
         <p>you are now in the params: {slug}</p>
+        <p>yes, i am now woking on the page </p>
     </div>
   )
 }
